f27f7ea52d877bed7f236c94ccd497d549e348d3
burmax00 <max.burgert@unibas.ch>
2019-03-28 10:58:17 +0100

    Separating game from gym

--- gym-block-world/gym_block_world/envs/block_world_env.py
+++ gym-block-world/gym_block_world/envs/block_world_env.py
@@ -1,59 +1,17 @@
 import gym
 from gym import error, spaces, distutils
 from gym.utils import seeding
-import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((88,80))
-done = False
-
-x = 2
-y = 2
-rect_size = 2
-move_distance = 1
-position_goal_x = 30
-position_goal_y = 30
-position_obstacle_x = 15
-position_obstacle_y = 10
-
-clock = pygame.time.Clock()
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_w]: y -= move_distance
-    if pressed[pygame.K_s]: y += move_distance
-    if pressed[pygame.K_a]: x -= move_distance
-    if pressed[pygame.K_d]: x += move_distance
-
-    if position_obstacle_x <= (x + rect_size) <= (position_obstacle_x + rect_size):
-        if position_obstacle_y <= (y + rect_size) <= (position_obstacle_y + rect_size):
-            done = True
-
-    if position_goal_x <= (x + rect_size) <= (position_goal_x + rect_size):
-        if position_goal_y <= (y + rect_size) <= (position_goal_y + rect_size):
-            done = True
-
-    screen.fill((0, 0, 0))
-
-    pygame.draw.rect(screen, (200, 28, 0), pygame.Rect(position_obstacle_x, position_obstacle_y, rect_size, rect_size))
-    pygame.draw.rect(screen, (50, 255, 50), pygame.Rect(position_goal_x, position_goal_y, rect_size, rect_size))
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, rect_size, rect_size))
-
-    pygame.display.flip()
-    clock.tick(60)
 
 class BlockWorldEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        ...
+        self.done = 0
 
     def step(self, action):
-        ...
+        if self.done == 1:
+            print("Game Over")
 
     def reset(self):
         ...
