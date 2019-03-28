9c629959b3183408bf81684434726236f75e0e79
burmax00 <max.burgert@unibas.ch>
2019-03-28 23:10:18 +0100

    Overturned representation of state space

--- dqn.py
+++ dqn.py
@@ -1,20 +1,10 @@
 import tensorflow as tf
-import numpy as np
 
-game_color = np.array([88,80,74]).mean()
-
-def preprocess_obervations(obs):
-    img = obs[1:176:2, ::2]
-    img = img.mean(axis=2)
-    img[img==game_color] = 0
-    img = (img -128) / 128 -1
-    return img.reshape(88,80,1)
-
-input_height = 50
-input_width = 50
+input_height = 8
+input_width = 8
 input_channels = 1
 conv_n_maps = [32, 64, 64]
-conv_kernel_sizes = [(8,8),(4,4),(3,3)]
+conv_kernel_sizes = [(8, 8), (4, 4), (3, 3)]
 conv_strides = [4, 2, 1]
 conv_paddings = ["SAME"] * 3
 conv_activation = [tf.nn.relu] * 3
@@ -24,15 +14,20 @@ hidden_activation = tf.nn.relu
 n_outputs = 4
 initializer = tf.contrib.layers.variance_scaling_initializer()
 
+
 def q_network(X_state, name):
     prev_layer = X_state
     with tf.variable_scope(name) as scope:
-        for n_maps, kernel_size, strides, padding, activation in zip(conv_n_maps, conv_kernel_sizes, conv_strides, conv_paddings, conv_activation):
-            prev_layer = tf.layers.conv2d(prev_layer, filters=n_maps,kernel_size=kernel_size,strides=strides,padding=padding,activation=activation,kernel_initializer=initializer)
-            last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1,n_hidden_in])
-            hidden = tf.layers.dense(last_conv_layer_flat, n_hidden, activation=hidden_activation,kernel_initializer=initializer)
+        for n_maps, kernel_size, strides, padding, activation in zip(conv_n_maps, conv_kernel_sizes, conv_strides,
+                                                                     conv_paddings, conv_activation):
+            prev_layer = tf.layers.conv2d(prev_layer, filters=n_maps, kernel_size=kernel_size, strides=strides,
+                                          padding=padding, activation=activation, kernel_initializer=initializer)
+            last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1, n_hidden_in])
+            hidden = tf.layers.dense(last_conv_layer_flat, n_hidden, activation=hidden_activation,
+                                     kernel_initializer=initializer)
             outputs = tf.layers.dense(hidden, n_outputs, kernel_initializer=initializer)
-        trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,scope=scope.name)
+
+        trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=scope.name)
         trainable_vars_by_name = {var.name[len(scope.name):]: var for var in trainable_vars}
 
-        return  outputs, trainable_vars_by_name
+        return outputs, trainable_vars_by_name
--- gym-block-world/gym_block_world/envs/block_world_env.py
+++ gym-block-world/gym_block_world/envs/block_world_env.py
@@ -2,6 +2,7 @@ import gym
 import random
 from gym import spaces
 
+
 # Example state
 # - - - - - - - - -
 # - o - - - - - - -
@@ -13,14 +14,23 @@ from gym import spaces
 # - - - - - - - - -
 # - - - - - - - - -
 
-class StateSpace:
 
-    def __init__(self, width, length):
-        self.width = width
-        self.length = length
+class BlockWorldEnv(gym.Env):
+    metadata = {'render.modes': ['human']}
+
+    def __init__(self):
+        self.done = 0
+        self.counter = 0
+        self.width = 8
+        self.height = 8
         self.pos_agent = self.calc_pos_agent()
         self.pos_goal = self.calc_pos_goal()
         self.pos_obstacle = self.calc_pos_obstacle()
+        # [shape,agent_pos,obstacle_pos,goal_pos]
+        self.state = [(self.width, self.height), self.pos_agent, self.pos_goal, self.pos_obstacle]
+        self.reward = 0
+        self.add = [0, 0]
+        self.action_space = spaces.Discrete(4)
 
     def calc_pos_agent(self):
         return self._calc_pos()
@@ -39,10 +49,10 @@ class StateSpace:
 
     def _calc_pos(self):
         pos_x = random.randint(0, self.width)
-        pos_y = random.randint(0, self.length)
+        pos_y = random.randint(0, self.height)
         return pos_x, pos_y
 
-    def apply_action(self, action):
+    def _apply_action(self, action):
         """
         :param action: possible actions: 0 -> up
                                          1 -> right
@@ -50,47 +60,37 @@ class StateSpace:
                                          3 -> left
         :return:
         """
+        state_copy = self.state.copy()
         if action == 0:
-            return self.pos_agent[0] - 1, self.pos_agent[1]
+            state_copy[1] = (state_copy[1][0] - 1), state_copy[1][1]
         elif action == 1:
-            return self.pos_agent[0], self.pos_agent[1] + 1
+            state_copy[1] = state_copy[1][0], (state_copy[1][1] + 1)
         elif action == 2:
-            return self.pos_agent[0] + 1, self.pos_agent[1]
+            state_copy[1] = (state_copy[1][0] + 1), state_copy[1][1]
         elif action == 3:
-            return self.pos_agent[0], self.pos_agent[1] - 1
-
-
-class BlockWorldEnv(gym.Env):
-    metadata = {'render.modes': ['human']}
-
-    def __init__(self):
-        self.done = 0
-        self.counter = 0
-        self.state = StateSpace(8, 8)
-        self.reward = 0
-        self.add = [0, 0]
-        self.action_space = spaces.Discrete(4)
+            state_copy[1] = state_copy[1][0], (state_copy[1][1] - 1)
+        return state_copy
 
     def step(self, action):
         if self.done == 1:
             print("Game Over")
             return [self.state, self.reward, self.done, self.add]
         else:
-            new_state = self.state.apply_action(action)
-            if new_state[0] < 0 or new_state[0] > 8 or new_state[1] < 0 or new_state[1] > 8:
+            new_state = self._apply_action(action)
+            if new_state[1][0] < 0 or new_state[1][0] > 8 or new_state[1][0] < 0 or new_state[1][0] > 8:
                 print("Invalid Step")
                 return [self.state, self.reward, self.done, self.add]
             else:
-                self.state.pos_agent = new_state
+                self.state = new_state
                 self.counter += 1
                 if self.counter >= 16:
                     self.done = 1
-                self.render()
+                # self.render()
 
         win = 0
-        if self.state.pos_agent == self.state.pos_goal:
+        if self.state[1] == self.state[2]:
             win = 1
-        elif self.state.pos_agent == self.state.pos_obstacle:
+        elif self.state[1] == self.state[3]:
             win = -1
 
         if win:
@@ -107,7 +107,11 @@ class BlockWorldEnv(gym.Env):
     def reset(self):
         self.done = 0
         self.counter = 0
-        self.state = StateSpace(8, 8)
+        self.pos_agent = self.calc_pos_agent()
+        self.pos_goal = self.calc_pos_goal()
+        self.pos_obstacle = self.calc_pos_obstacle()
+        # [shape,agent_pos,goal_pos,obstacle_pos]
+        self.state = [(self.width, self.height), self.pos_agent, self.pos_goal, self.pos_obstacle]
         self.reward = 0
         self.add = [0, 0]
         return self.state
@@ -115,11 +119,11 @@ class BlockWorldEnv(gym.Env):
     def render(self, mode='human', close=False):
         for i in range(8 + 1):
             for j in range(8 + 1):
-                if self.state.pos_agent == (i, j):
+                if self.state[1] == (i, j):
                     print('x', end=' ')
-                elif self.state.pos_goal == (i, j):
+                elif self.state[2] == (i, j):
                     print('o', end=' ')
-                elif self.state.pos_obstacle == (i, j):
+                elif self.state[3] == (i, j):
                     print('#', end=' ')
                 else:
                     print('-', end=' ')
